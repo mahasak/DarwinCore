2ced59baf1b51bf8dccfb6fed13c95d049d56a2a
Max Pijittum <mpijittum@fb.com>
2021-02-28 01:56:17 +0800

    Delete cache class

--- Shared/Cache/ICacheManager.cs
+++ Shared/Cache/ICacheManager.cs
@@ -1,6 +1,15 @@
 namespace Darwin.Shared.Cache 
 {
-    public interface ICacheManager<TCacheValue> {
-
+    using System;
+    using Darwin.Shared.Cache.Model;
+    public interface ICacheManager<TCacheType> {
+        void Clear();
+        void Add(string key, TCacheType data);
+        void Add(string Key, TCacheType data, Func<TCacheType> UpdateOnExpire);
+        bool Update(string key, TCacheType data);
+        bool LookUp(string key, bool localOnly = false);
+        CacheItem<TCacheType> Remove(string key, bool localOnly = false);
+        TCacheType Get(string key, bool localOnly = false);
+        event EmptyCacheHandler EmptyCacheEvent;
     }
 }
