904d5b7216154610358324667b7e71b79fc5dc93
Max Pijittum <mpijittum@fb.com>
2021-02-28 01:57:13 +0800

    Implement blank cache class

--- Shared/Cache/CacheManager.cs
+++ Shared/Cache/CacheManager.cs
@@ -10,5 +10,42 @@ namespace Darwin.Shared.Cache
         private static ConcurrentDictionary<string, CacheItem<TCacheType>> _localCache;
         private ExpirationMode _mode;
         private TimeSpan _timeout;
+
+        public event EmptyCacheHandler EmptyCacheEvent;
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(string key, TCacheType data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(string Key, TCacheType data, Func<TCacheType> UpdateOnExpire)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(string key, TCacheType data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LookUp(string key, bool localOnly = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CacheItem<TCacheType> Remove(string key, bool localOnly = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TCacheType Get(string key, bool localOnly = false)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
